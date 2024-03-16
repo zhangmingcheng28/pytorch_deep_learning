557e03beb51dd53cd6b760cd32a8265dcbdcb7c9
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2024-03-16 19:25:25 +0800

    MLP model for prediction

--- d2l-zh/pytorch/energy_MLPV2.py
+++ d2l-zh/pytorch/energy_MLPV2.py
@@ -21,6 +21,7 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 import time
+import csv
 import math
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -54,11 +55,13 @@ def column_index(df, query_cols):
 
 
 def get_dataset(x, y):
-    return TensorDataset(torch.from_numpy(x).float(), torch.from_numpy(y).float())
+    return TensorDataset(torch.from_numpy(x).float().to(device), torch.from_numpy(y).float().to(device))
 
 
-def get_dataloader(x: np.array, y: np.array, batch_size: int, shuffle: bool = True, num_workers: int = 0):
-    dataset = get_dataset(x, y)  # convert the numpy to torch standard TensorDataset
+def get_dataloader(x: np.array, y: np.array, batch_size: int, shuffle: bool = True, num_workers: int = 0, name=None):
+    dataset = get_dataset(x, y)  # convert the numpy to torch standard TensorDataset.
+    if name == 'test':
+        return DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)  # load and prepare the DataSet
 
 
@@ -85,25 +88,24 @@ def df_to_xyArray(inputDF, columnPos, max_colVal, min_colVla):  # convert the df
     columnIdx_name_ref = {}
     # only choose part of the dataframe
     features = inputDF[
-        ['mass', 'roll', 'pitch', 'yaw', 'roll_rate', 'pitch_rate', 'yaw_rate', 'v_n', 'v_e', 'v_d', 'accel_n', 'accel_e', 'accel_d', 'wind_n', 'wind_e', 'energy']]  # only exclude "flight"
+        ['flight', 'mass', 'roll', 'pitch', 'yaw', 'roll_rate', 'pitch_rate', 'yaw_rate', 'v_n', 'v_e', 'v_d', 'accel_n', 'accel_e', 'accel_d', 'wind_n', 'wind_e', 'energy']]  # only exclude "flight"
     featuresArray = features.values  # extract the values in DF, in terms of 2D array, same size with DF
     # normalise
     featuresArray[:, columnPos] = (featuresArray[:, columnPos] - min_colVla) / (max_colVal - min_colVla)  # normalise every flight's required column's data
-    x_input = featuresArray[:, :-1].astype(float)  # set the input of the flight, excluding the last column of power
-    supposed_y_output = featuresArray[:, -1].astype(float)
-    for columnIdex in range(0, featuresArray.shape[1]):
+    # Now, we can drop the 1st column, as the 1st column represent "flight" number
+    featuresArray_dropFlight = np.delete(featuresArray,0,axis=1)
+    x_input = featuresArray_dropFlight[:, :-1].astype(float)  # set the input of the flight, excluding the last column of power
+    supposed_y_output = featuresArray_dropFlight[:, -1].astype(float)
+    for columnIdex in range(0, featuresArray_dropFlight.shape[1]):
         columnIdx_name_ref[columnIdex] = features.columns[columnIdex]
-    x, y = multivariate_data(x_input, supposed_y_output, single_step=True)
-    # Transpose the x, so that features is the row, time-sequence/step is column
-    x = np.transpose(x, (0, 2, 1))  # 0th dimension stay at the 1st place, the 1st and 2nd dimension chance place.
-    return x, y, columnIdx_name_ref
+    return x_input, supposed_y_output, columnIdx_name_ref
 
 
-def load_xyDict_to_dataloaderDict(inputDict):
+def load_xyDict_to_dataloaderDict(inputDict, name):
     dataLoaderDict = {}
     batchSize = 32
     for flightIdx_key, xyArray in inputDict.items():
-        dataLoaderDict[flightIdx_key] = get_dataloader(xyArray[0], xyArray[1], batchSize)
+        dataLoaderDict[flightIdx_key] = get_dataloader(xyArray[0], xyArray[1], batchSize, name=name)
     return dataLoaderDict
 
 # ======================
@@ -142,8 +144,8 @@ for eachFlight in train_list:
         x, y, idx_name_ref = df_to_xyArray(eachFlight, ColumnToNormalise, data_max, data_min)
         test_data_dict[eachFlight['flight'].iloc[0]] = (x, y)
 # load the data dictionary to dataloader and store as dictionary for every flight
-dataLoader_training_Dict = load_xyDict_to_dataloaderDict(train_data_dict)
-dataLoader_test_Dict = load_xyDict_to_dataloaderDict(test_data_dict)
+dataLoader_training_Dict = load_xyDict_to_dataloaderDict(train_data_dict, name='training')
+dataLoader_test_Dict = load_xyDict_to_dataloaderDict(test_data_dict, name='test')  # here load data into dataloader without shuffle
 
 # ======================
 #     Configure and load MLP model
@@ -171,9 +173,9 @@ def train(ep):
             # clear the gradients
             optimizer.zero_grad()
             # compute the model output
-            pred_out = model(data_in)  # data_in, is (N,C,L) format,N is batch size, C is number of features, L is the length of sequence (time-steps)
+            pred_out = model(data_in)
             # calculate loss
-            loss = criterion(pred_out.squeeze(), target)
+            loss = criterion(pred_out, target.unsqueeze(1))
             # weight assignment
             loss.backward()
             # update model weights
@@ -184,14 +186,15 @@ def train(ep):
 # ======================
 #     Evaluation loop
 # ======================
-def evaluate(X_data, name):
+def evaluate(X_data, epIdx, name):
     model.eval()
+    writer = pd.ExcelWriter('episode_' + str(epIdx) + '_MLP_model' + '.xlsx', engine='xlsxwriter')
     total_loss = 0.0
     Idx_list = None
     if name == "Validation":
         #Idx_list = validate_flightIdx
         Idx_list = None
-        np.random.shuffle(Idx_list)
+
     elif name == "Test":
         Idx_list = test_flightIdx
 
@@ -199,27 +202,67 @@ def evaluate(X_data, name):
     evaRecord_singleFlight = {}
     with torch.no_grad():
         for idx, flight in enumerate(Idx_list):
+            flightDataDF = pd.DataFrame()  # create a DF for a single sheet in excel
+            batchLength = None
             for batch_idx, (data_in, target) in enumerate(X_data[flight]):
                 # compute the model output
-                pred_out = model(data_in)  # data_in, is (N,C,L) format,N is batch size, C is number of features, L is the length of sequence (time-steps)
+                pred_out = model(data_in)  # data_in, is (batch_length, number of features)
                 # calculate loss
-                loss = criterion(pred_out.squeeze(), target)
+                loss = criterion(pred_out, target.unsqueeze(1))
                 total_loss += loss.item()
                 # record the (flight, batch_idx) together with data_in, target, and pred_out
-                evaRecord_singleFlight[(flight, batch_idx)] = [data_in, target, pred_out]
+                if flightDataDF.empty:
+                    flightDataDF.insert(0, 'Batch_'+str(batch_idx)+'actual_target', target.cpu().detach().numpy().reshape((len(target), 1)).tolist())
+                    flightDataDF.insert(len(flightDataDF.columns), 'Batch_' + str(batch_idx) + 'predict_val', pred_out.cpu().detach().numpy().tolist())
+                    batchLength = len(flightDataDF)
+                else:
+                    if len(target.cpu().detach().numpy().reshape((len(target), 1)).tolist()) != batchLength:
+                        addNanLength = batchLength - len(target.cpu().detach().numpy().reshape((len(target), 1)).tolist())
+
+                        addNan_flightTarget_arr = np.append(target.cpu().detach().numpy().reshape((len(target), 1)).tolist(), np.repeat(np.nan, addNanLength))
+                        addNan_flightTarget = addNan_flightTarget_arr.reshape(len(addNan_flightTarget_arr),1).tolist()
+
+                        addNan_flightPred_arr = np.append(pred_out.cpu().detach().numpy().tolist(), np.repeat(np.nan, addNanLength))
+                        addNan_flightPred = addNan_flightPred_arr.reshape(len(addNan_flightPred_arr),1).tolist()
+
+                        tar_to_combine = pd.DataFrame({'Batch_' + str(batch_idx) + '_actual_target': addNan_flightTarget})
+                        pred_to_combine = pd.DataFrame({'Batch_' + str(batch_idx) + '_predict_val': addNan_flightPred})
+                        flightDataDF = pd.concat((flightDataDF, tar_to_combine, pred_to_combine), axis=1)
+                    else:
+                        tar_to_combine = pd.DataFrame({'Batch_' + str(batch_idx) + '_actual_target': target.cpu().detach().numpy().reshape((len(target), 1)).tolist()})
+                        pred_to_combine = pd.DataFrame({'Batch_' + str(batch_idx) + '_predict_val': pred_out.cpu().detach().numpy().tolist()})
+                        flightDataDF = pd.concat((flightDataDF, tar_to_combine, pred_to_combine), axis=1)
+            flightDataDF.to_excel(writer, sheet_name='flight' + str(flight))
+        writer.save()
         return total_loss, evaRecord_singleFlight
 
 
-training_test_result_with_model = {}
+episode_trainLoss = []
+episode_evaLoss = []
+header = ['training loss per episode', 'evaluation loss per episode']
 for epIdx in range(1, total_epochs+1):
     begin = time.time()  #
     train_loss = train(epIdx)
     print(epIdx, train_loss)
-    tloss, evaRecord_singleFlight = evaluate(dataLoader_test_Dict, name='Test')
-    training_test_result_with_model[epIdx] = [tloss, evaRecord_singleFlight]
-    # torch.save(model.state_dict(), r'F:\githubClone\TCN\TCN\result_from_ownDataV2\epoch_' + str(epIdx)+'_model')
-    # with open(r'F:\githubClone\TCN\TCN\result_from_ownDataV2\epoch_' + str(epIdx) + '_loss_and_evaluationResult.pickle', 'wb') as handle:
-    #     pickle.dump(training_test_result_with_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    # epoch_time_used = time.time()-begin
+    tloss, evaRecord_singleFlight = evaluate(dataLoader_test_Dict, epIdx, name='Test')
+
+    episode_trainLoss.append(train_loss)
+    episode_evaLoss.append(tloss)
+
+    # torch.save(model.state_dict(), r'F:\githubClone\pytorch_deep_learning\d2l-zh\pytorch\MLP_energy_ownDataV2_result\epoch_' + str(epIdx)+'_model')
+    torch.save(model.state_dict(), r'F:\githubClone\pytorch_deep_learning\d2l-zh\pytorch\MLP_energy_ownDataV2_result_2024\epoch_' + str(epIdx)+'_model')
+    epoch_time_used = time.time()-begin
     print("epoch {} used {} second to finish".format(epIdx, epoch_time_used))
+
+
+# ======================
+#     Save loss per episode to CSV
+# ======================
+with open('2024_lossList_MLP_model.csv', 'wt', newline ='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(i for i in header)
+    c = 0
+    for _ in episode_trainLoss:
+        writer.writerow([episode_trainLoss[c], episode_evaLoss[c]])
+        c = c + 1
 print("end of all epochs!")
